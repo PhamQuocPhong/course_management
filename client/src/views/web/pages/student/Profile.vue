e07532d9cdcdd96dd870926e14b5a326dc053797
tranvuongit97 <19424010@student.hcmus.edu.vn>
2021-03-18 22:10:48 +0700

    view info, mycourse, profile, rewiew of student

---
+++ client/src/views/web/pages/student/Profile.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main-layout pa-3">
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  mounted() {}
+};
+</script>
